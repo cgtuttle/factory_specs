a97f3a5824302ae9af67ab3c43e6357e49244717
Carl Tuttle <cgtuttle@gmail.com>
2012-04-01 09:44:18 -0500

    Removed new item_spec line in index

--- app/controllers/item_specs_controller.rb
+++ app/controllers/item_specs_controller.rb
@@ -2,11 +2,11 @@ class ItemSpecsController < ApplicationController
 	
   def index
 		set_scope
-		@title = 'Item Specifications'
 		if params[:commit] =='Apply'
 			@item = Item.find(params[:item_spec][:item_id])
 		end
-		@item_specs = ItemSpec.where(:item_id => @item.id)
+		@title = "Specifications for #{@item.code}"
+		@item_specs = ItemSpec.joins(:spec).where(:item_id => @item.id).order('specs.category', 'specs.display_order')
 		@specs = Spec.order(:code)
 		@items = Item.order(:code)
   end
@@ -17,12 +17,6 @@ class ItemSpecsController < ApplicationController
 		@item_spec = ItemSpec.new(@existing_item_spec.attributes)
 		@item_spec.eff_date = Date.today
 	end
-	
-	def destroy
-		ItemSpec.find(params[:id]).destroy
-		flash[:success] = "Item Spec deleted"
-		redirect_to item_specs_path
-  end
 	
 	def set_scope
 		

e0329207f86d4361286acda2334abfad5d0e749c
Carl Tuttle <cgtuttle@gmail.com>
2012-04-22 16:49:58 -0500

    Precompiled...

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,3 @@
 class MainController < ApplicationController
-  def contents
-		@items = Item.order('code').all
-  end
-	
-	def display
-		redirect_to display_item_path(params[:selected_item])
-	end
-
+  
 end

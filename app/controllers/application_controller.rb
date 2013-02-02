97e56783e3060d9ee991e41d63aae37e32ae66bd
Carl Tuttle <cgtuttle@gmail.com>
2013-02-02 09:29:11 -0600

    Fixed hole in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
 	before_filter :set_dflt_item
 	before_filter :set_dflt_content_id
 	
